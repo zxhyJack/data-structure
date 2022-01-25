59f4cdf8483811c00a737b9e2b2b07e9217ad720
zxhyJack <zxhyjack@gmail.com>
2022-01-25 16:25:47 +0800

    delete: .vscode

--- go/singleLinkedList/singleLinkedList_test.go
+++ go/singleLinkedList/singleLinkedList_test.go
@@ -7,27 +7,29 @@ func TestList(t *testing.T) {
 		head: &Node{data: 1},
 		len:  1,
 	}
-	t.Logf("%+v", list)
 
 	list.AddFront(2)
 	list.AddFront(3)
 	list.AddFront(4)
 	list.AddFront(5)
 
+	tmp := *list.head
+	t.Log(tmp)
+
 	t.Log(list.Traverse())
-	t.Log(list.Size())
+	// t.Log(list.Size())
 
-	if err := list.Insert(3, 1); err != nil {
-		t.Log(err)
-	}
-	t.Log(list.Traverse())
+	// if err := list.Insert(3, 1); err != nil {
+	// 	t.Log(err)
+	// }
+	// t.Log(list.Traverse())
 
-	if _, err := list.RemoveAt(0); err != nil {
-		t.Log(err)
-	}
-	t.Log(list.Traverse())
+	// if _, err := list.RemoveAt(0); err != nil {
+	// 	t.Log(err)
+	// }
+	// t.Log(list.Traverse())
 
-	t.Log(list.Indexof(5))
+	// t.Log(list.Indexof(5))
 
-	t.Log(list.Size2())
+	// t.Log(list.Size2())
 }
