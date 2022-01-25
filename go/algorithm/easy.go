1d63586d36f7119032f0425c415167ce127e9eff
zxhyJack <zxhyjack@gmail.com>
2022-01-25 16:11:44 +0800

    merge two sorted linkedlist

--- go/algorithm/easy.go
+++ go/algorithm/easy.go
@@ -1,7 +1,5 @@
 package algorithm
 
-import "fmt"
-
 // 两数之和
 // hashmap
 func twoSum(nums []int, target int) []int {
@@ -44,24 +42,28 @@ type ListNode struct {
 	Next *ListNode
 }
 
+// ! 值传递只是修改数据的副本
+// ! 指针传递是修改原始数据
+// ! 函数传递和变量传递均是如此
 // 链表反转1
 func reverseList(head *ListNode) *ListNode {
 	current := head
-	head = nil
+	var reverse *ListNode
 
 	for ; current != nil; current = current.Next {
 		// 取下原链表的第一个节点
-		node := *current // 拿到的是节点而不是指针，否则对node指针操作会影响current
+		node := *current // 拿到的是节点的**副本**而不是指针，否则对node指针操作会影响current
 		// 将取下的节点放入第二个链表（头插法）
-		node.Next = head
-		head = &node
+		node.Next = reverse
+		reverse = &node
 	}
 
-	return head
+	return reverse
 
 }
 
 // 链表反转
+// 对同一个链表操作，当前节点的Next指针指向前一个节点
 func reverseList2(head *ListNode) *ListNode {
 	current := head
 	var prev *ListNode
@@ -74,9 +76,66 @@ func reverseList2(head *ListNode) *ListNode {
 	return prev
 }
 
-func traverse(head *ListNode) {
-	for ; head != nil; head = head.Next {
-		fmt.Printf("%d ", head.Val)
+// 两有序链表重组1
+// 不修改原链表，新建节点放到新链表中
+func mergeTwoLists1(list1 *ListNode, list2 *ListNode) *ListNode {
+	var dummy = &ListNode{0, nil} // 虚拟头，解决空指针的问题
+	new := dummy
+	for list1 != nil && list2 != nil {
+		if list1.Val == list2.Val {
+			new.Next = &ListNode{list1.Val, nil}
+			new = new.Next
+
+			new.Next = &ListNode{list2.Val, nil}
+			new = new.Next
+
+			list1 = list1.Next
+			list2 = list2.Next
+		} else if list1.Val < list2.Val {
+			new.Next = &ListNode{list1.Val, nil}
+			new = new.Next
+			list1 = list1.Next
+		} else {
+			new.Next = &ListNode{list2.Val, nil}
+			new = new.Next
+			list2 = list2.Next
+		}
 	}
-	fmt.Println()
+	for list1 != nil {
+		new.Next = &ListNode{list1.Val, nil}
+		new = new.Next
+		list1 = list1.Next
+	}
+	for list2 != nil {
+		new.Next = &ListNode{list2.Val, nil}
+		new = new.Next
+		list2 = list2.Next
+	}
+	return dummy.Next
+}
+
+// 两有序链表重组2
+// 无需新建节点，直接在原链表上操作
+func mergeTwoLists2(list1 *ListNode, list2 *ListNode) *ListNode {
+	dummy := &ListNode{0, nil}
+	p := dummy
+
+	for list1 != nil && list2 != nil {
+		if list1.Val <= list2.Val {
+			p.Next = list1
+			p = p.Next // 这一行可以放在if判断后面，两种情况都需要移动指针
+			list1 = list1.Next
+		} else {
+			p.Next = list2
+			p = p.Next //
+			list2 = list2.Next
+		}
+	}
+
+	if list1 != nil {
+		p.Next = list1
+	} else {
+		p.Next = list2
+	}
+	return dummy.Next
 }
--- go/algorithm/easy_test.go
+++ go/algorithm/easy_test.go
@@ -1,13 +1,62 @@
 package algorithm
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAlgorithm(t *testing.T) {
-	head := &ListNode{1, nil}
-	head.Next = &ListNode{2, &ListNode{3, nil}}
-	traverse(head)
-	reverse := reverseList(head)
+	list1 := &ListNode{1, &ListNode{2, &ListNode{3, nil}}}
+	traverse(list1)
+	reverse := reverseList(list1)
 	traverse(reverse)
-	reverse2 := reverseList2(head)
+	reverse2 := reverseList2(list1)
 	traverse(reverse2)
+}
+
+func traverse(head *ListNode) {
+	for ; head != nil; head = head.Next {
+		fmt.Printf("%d ", head.Val)
+	}
+	fmt.Println()
+}
+
+func foo() {
+	list1 := &ListNode{1, &ListNode{2, &ListNode{3, nil}}}
+	list2 := &ListNode{1, &ListNode{3, &ListNode{4, nil}}}
+
+	curr1 := list1
+	curr2 := list2
+
+	fmt.Println(curr1)
+	fmt.Println(curr2)
+}
+
+func TestList(t *testing.T) {
+	list := &ListNode{1, &ListNode{2, &ListNode{3, nil}}}
+	var head *ListNode
+	cur := list
+	for cur != nil {
+		node := cur
+		cur = cur.Next
+		node.Next = head
+		head = node
+	}
+	traverse(head)
+}
+
+func TestMergeTwoLists1(t *testing.T) {
+	list1 := &ListNode{1, &ListNode{2, &ListNode{3, nil}}}
+	list2 := &ListNode{1, &ListNode{3, &ListNode{4, nil}}}
+
+	list3 := mergeTwoLists1(list1, list2)
+	traverse(list3)
+}
+
+func TestMergeTwoLists2(t *testing.T) {
+	list1 := &ListNode{1, &ListNode{2, &ListNode{3, nil}}}
+	list2 := &ListNode{1, &ListNode{3, &ListNode{4, nil}}}
+
+	list3 := mergeTwoLists2(list1, list2)
+	traverse(list3)
 }
